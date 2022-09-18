8733680f7863042dc300d7261f201a885d763881
pedrorosadomingues <pedrorosadomingues@gmail.com>
2022-09-17 22:51:59 -0300

    Update index.jsx

--- primeiro-projeto-react/src/Components/Header/index.jsx
+++ primeiro-projeto-react/src/Components/Header/index.jsx
@@ -13,25 +13,24 @@ function Header({ pageState, handlePageState }) {
         ></img>
         <div id="cabecalho">
 
-          <button className='publicSans400 height14 size12 letterSpacing2 black1' 
+          <button className='publicSans400 height14 size12 letterSpacing2 black1'
             onClick={() => handlePageState('home')}
             style={{ color: pageState === 'home' ? '#5FB4A2 ' : '#000000' }}>
             HOME
           </button>
 
-          <button className='publicSans400 height14 size12 letterSpacing2 black1' 
+          <button className='publicSans400 height14 size12 letterSpacing2 black1'
             onClick={() => handlePageState('portfolio')}
             style={{ color: pageState === 'portfolio' ? '#5FB4A2 ' : '#000000' }}>
             PORTFÓLIO
           </button>
 
-          <button className='publicSans400 height14 size12 letterSpacing2 black1' 
+          <button className='publicSans400 height14 size12 letterSpacing2 black1'
             onClick={() => handlePageState('contato')}
             style={{ color: pageState === 'contato' ? '#5FB4A2 ' : '#000000' }}>
             CONTATO
           </button>
         </div>
-
       </header>
     </div>
   );
